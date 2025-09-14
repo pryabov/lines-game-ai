fde8a7ff72b3b6dcad6bf71366b2d0e65829fbc5
Pavel Rabau <prpbox@outlook.com>
2025-09-14 18:20:36 +0200

    Use fontawesome instead of swg

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { useLanguage } from './hooks/useLanguage';
 import './App.scss';
 import './styles/DarkTheme.scss';
 import './styles/SettingsDialog.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 // Wrap the app content in the LanguageContext
 const AppContent: React.FC = () => {
@@ -27,17 +28,14 @@ const AppContent: React.FC = () => {
       <header className="App-header">
         <h1>{t.header.title}</h1>
         <div className="header-right">
-          {/* <LanguageSelector /> */}
-          {/* <ThemeToggle /> */}
-          TEST
+          <LanguageSelector />
+          <ThemeToggle />
           <button 
             className="settings-button" 
             onClick={openSettings}
             aria-label="Open settings"
           >
-            <svg className="settings-icon" viewBox="0 0 24 24">
-              <path d="M12 15.5A3.5 3.5 0 0 1 8.5 12A3.5 3.5 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5 3.5 3.5 0 0 1-3.5 3.5m7.43-2.53c.04-.32.07-.64.07-.97 0-.33-.03-.66-.07-1l2.11-1.63c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.31-.61-.22l-2.49 1c-.52-.39-1.06-.73-1.69-.98l-.37-2.65A.506.506 0 0 0 14 2h-4c-.25 0-.46.18-.5.42l-.37 2.65c-.63.25-1.17.59-1.69.98l-2.49-1c-.22-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64L4.57 11c-.04.34-.07.67-.07 1 0 .33.03.65.07.97L2.46 14.6c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.31.61.22l2.49-1c.52.39 1.06.73 1.69.98l.37 2.65c.04.24.25.42.5.42h4c.25 0 .46-.18.5-.42l.37-2.65c.63-.25 1.17-.59 1.69-.98l2.49 1c.22.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64L19.43 13Z"/>
-            </svg>
+            <i className="fas fa-cog settings-icon"></i>
           </button>
         </div>
       </header>
